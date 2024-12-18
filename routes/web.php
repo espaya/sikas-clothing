ab98eba8c49987ff762ba5480296c4b5785c465f
espaya <beehivelive8@gmail.com>
2024-12-18 11:54:34 +0000

    Added about page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\AccountDetailsController;
 use App\Http\Controllers\AddressController;
 use App\Http\Controllers\Auth\LoginController;
@@ -12,18 +13,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'guest'], function(){
     
-    Route::get('/', function () {
-        return view('index');
-    })->name('homepage');
-    
     Route::get('/myaccount/login', [LoginController::class, 'showLoginForm'])->name('login.view');
     Route::post('/myaccount/login', [LoginController::class, 'login'])->name('login');
-    Route::post('/myaccount/logout', [LoginController::class, 'logout'])->name('logout');
-    
     Route::get('/myaccount/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
     // Route::post('/myaccount/register', [RegisterController::class, 'register'])->name('register');
     
 });
+
+Route::get('/', function () {
+    return view('index');
+})->name('homepage');
+
+Route::get('/about', [AboutController::class, 'index'])->name('about');
+Route::post('/myaccount/logout', [LoginController::class, 'logout'])->name('logout');
+
 
 
 Route::get('/myaccount/2fa-setup', [TwoFactorController::class, 'showForms'])->name('2fa.show');
